133b6d13c8cabf1cf9c8ff9121b7c932f31ce848
StanleyDev11 <stanleyakm29@gmail.com>
2025-11-20 23:42:47 +0000

    fix: Correction du graphique du tableau de bord et mise à jour des templates

--- src/main/java/com/example/boutique/dto/MouvementStatDto.java
+++ src/main/java/com/example/boutique/dto/MouvementStatDto.java
@@ -1,41 +1,93 @@
 package com.example.boutique.dto;
 
+
+
 import java.time.LocalDate;
+
 import java.sql.Date; // Import java.sql.Date
 
+import java.time.LocalDateTime;
+
+
+
 public class MouvementStatDto {
+
     private LocalDate date;
-    private long count;
+
+    private Long count;
+
+
 
     // Default constructor for JPA
+
     public MouvementStatDto() {
+
     }
 
-    public MouvementStatDto(LocalDate date, long count) {
+
+
+    public MouvementStatDto(LocalDate date, Long count) {
+
         this.date = date;
+
         this.count = count;
+
     }
+
+
 
     // New constructor to handle java.sql.Date from the query
-    public MouvementStatDto(Date sqlDate, long count) {
+
+    public MouvementStatDto(Date sqlDate, Long count) {
+
         this.date = sqlDate.toLocalDate(); // Convert to LocalDate
+
         this.count = count;
+
     }
+
+
+
+    public MouvementStatDto(LocalDateTime dateTime, Long count) {
+
+        this.date = dateTime.toLocalDate();
+
+        this.count = count;
+
+    }
+
+
 
     // Getters and Setters
+
     public LocalDate getDate() {
+
         return date;
+
     }
+
+
 
     public void setDate(LocalDate date) {
+
         this.date = date;
+
     }
 
-    public long getCount() {
+
+
+    public Long getCount() {
+
         return count;
+
     }
 
-    public void setCount(long count) {
+
+
+    public void setCount(Long count) {
+
         this.count = count;
+
     }
+
 }
